a22a524c8d72b5b62184aa3a91af44c20c9d45be
Ayasterisk <ayasterisk@gmail.com>
2024-12-16 09:21:00 +0700

    fix

--- predict.py
+++ predict.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO('D:/GitHub/YOLOv8_FireDetection/runs/detect/train/weights/best.pt')
+model = YOLO('runs/detect/train/weights/best.pt')
 
-result = model.predict(source='D:/GitHub/YOLOv8_FireDetection/fire_detection_test.mp4',
+result = model.predict(source='fire_detection_test.mp4',
                        show=True, save=False)
